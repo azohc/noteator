2668e065cf0fb490f8009985fec868432bca25b1
chosapesé <chosapes@DESKTOP-1BPO8I7>
2018-05-13 19:21:48 +0200

    zzz

--- src/components/Banner.java
+++ src/components/Banner.java
@@ -6,8 +6,6 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Banner {
 	public String generateBanner(String s) {
